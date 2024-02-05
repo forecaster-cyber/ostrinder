6cd29f500298f0fec378d833142720b3d0eb177c
Serge Brainin <forecaster1310@gmail.com>
2024-02-05 22:48:24 +0200

    update

--- lib/calls.dart
+++ lib/calls.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'main.dart';
 
 bool open = false;
+bool isWebMobile = kIsWeb &&
+    (defaultTargetPlatform == TargetPlatform.iOS ||
+        defaultTargetPlatform == TargetPlatform.android);
 
 class calls_screen extends StatefulWidget {
   const calls_screen({super.key});
@@ -18,7 +22,7 @@ class calls_screen extends StatefulWidget {
 class _calls_screenState extends State<calls_screen> {
   final stream = supabase
       .from("rooms")
-      .stream(primaryKey: ["room_num"]).order("room_num", ascending: true);
+      .stream(primaryKey: ["room_num"]).order("open", ascending: false);
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +32,7 @@ class _calls_screenState extends State<calls_screen> {
           SvgPicture.asset(
             'assets/images/bruh.svg',
             alignment: Alignment.center,
-            fit: BoxFit.cover,
+            fit: BoxFit.fill,
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
           ),
@@ -37,14 +41,14 @@ class _calls_screenState extends State<calls_screen> {
             builder: (context, snapshot) {
               List<Map<String, dynamic>>? datatatata = snapshot.data;
               return GridView.count(
-                crossAxisCount: 3,
+                crossAxisCount: isWebMobile ? 1 : 3,
                 children: List.generate(12, (index) {
                   return Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX:0, sigmaY: 0),
+                        filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
                         child: Center(
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
@@ -58,7 +62,8 @@ class _calls_screenState extends State<calls_screen> {
                                   begin: AlignmentDirectional.topStart,
                                   end: AlignmentDirectional.bottomEnd,
                                 ),
-                                borderRadius: BorderRadius.all(Radius.circular(7)),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(7)),
                                 border: Border.all(
                                   width: 1.5,
                                   color: Colors.white.withOpacity(0.6),
@@ -66,7 +71,12 @@ class _calls_screenState extends State<calls_screen> {
                               ),
                               child: Column(
                                 children: [
-                                  Text((index + 1).toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0),),
+                                  Text(
+                                    datatatata![index]['name'].toString(),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 50.0),
+                                  ),
                                   GestureDetector(
                                     child: Image(
                                         image: AssetImage(
@@ -77,32 +87,53 @@ class _calls_screenState extends State<calls_screen> {
                                         final data = await supabase
                                             .from('rooms')
                                             .select('open')
-                                            .eq("room_num", index);
+                                            .eq("name",
+                                                datatatata![index]['name']);
                                         print(data);
-                                        await supabase
-                                            .from('rooms')
-                                            .update({'open': !data[0]['open']}).match(
-                                                {'room_num': index});
+
+                                        await supabase.from('rooms').update(
+                                            {'open': !data[0]['open']}).match({
+                                          'name': datatatata![index]['name']
+                                        });
                                       } else {
                                         print("unauthorized");
+                                        print(kIsWeb);
                                       }
                                     },
                                   ),
                                   ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: datatatata![index]
+                                                ['open']
+                                            ? Colors.green
+                                            : Colors.red),
                                     onPressed: () {
                                       if (datatatata![index]['open']) {
-                                        launchUrl(
-                                            Uri.parse(datatatata![index]['link']));
+                                        launchUrl(Uri.parse(
+                                            datatatata![index]['link']));
                                       } else {
                                         ScaffoldMessenger.of(context)
                                             .showSnackBar(SnackBar(
-                                          content: Text("החדר לא פתוח"),
+                                          content: Text(
+                                            "החדר לא פתוח",
+                                          ),
                                           duration: Duration(milliseconds: 300),
                                         ));
                                       }
                                     },
-                                    child: Text(
-                                        datatatata![index]['open'] ? "פתוח" : "סגור"),
+                                    child: Padding(
+                                      padding: isWebMobile
+                                          ? const EdgeInsets.all(0.0)
+                                          : const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        datatatata![index]['open']
+                                            ? "החדר זמין"
+                                            : "החדר לא זמין",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: isWebMobile ? 22.5 : 45),
+                                      ),
+                                    ),
                                   )
                                 ],
                               ),
