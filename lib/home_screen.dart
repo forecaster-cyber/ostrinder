3c73300ef42f6209ab91cf43405ce19b6f421340
Serge Brainin <forecaster1310@gmail.com>
2024-02-14 19:11:31 +0200

    avaliablity

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_boring_avatars/flutter_boring_avatars.dart';
 import 'package:ostrinder/main.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'calls.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -20,58 +21,67 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         extendBodyBehindAppBar: true,
         appBar: PreferredSize(
-          preferredSize: Size(MediaQuery.of(context).size.width, 100),
-          child: ClipRRect(
-            child: BackdropFilter(
+            preferredSize: Size(MediaQuery.of(context).size.width, 100),
+            child: ClipRRect(
+                child: BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-          child: AppBar(
-            
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            title: Text("כפר עזרה"),
-          
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    if (supabase.auth.currentUser == null) {
-                      Navigator.pushNamed(context, '/sign', arguments: "profile")
-                          .then((value) => setState(() {}));
-                    } else {
-                      Navigator.pushNamed(context, '/profile')
-                          .then((value) => setState(() {}));
-                    }
-                  },
-                  icon: supabase.auth.currentSession == null
-                      ? CircleAvatar(
-                          child: Icon(Icons.person),
-                          backgroundColor: Colors.white,
-                          radius: 100,
-                        )
-                      : BoringAvatars(name: supabase.auth.currentUser!.email!)),
-              IconButton(
-                  onPressed: () {
-                    launchUrl(
-                        Uri.parse("https://github.com/forecaster-cyber/Meshik"));
-                  },
-                  icon: Icon(Icons.info))
-            ],
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  text: 'לוח שאלות',
+              child: AppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                title: Text("כפר עזרה"),
+                actions: [
+                  Padding(
+                    padding:  EdgeInsets.only(right: ((MediaQuery.of(context).size.width)/2)-250, left: 0.0),
+                    child: TextButton(
+                        onPressed: () {
+                          launchUrl(Uri.parse(
+                              "https://docs.google.com/spreadsheets/d/199ajAA3xPpAQeUbqYiaRYCVYL0jM3TWnfaTf8uafamM/edit?usp=sharing"));
+                        },
+                        child: Text("לחצו כאן לזמינות המתנדבים", style: TextStyle(fontSize: isWebMobile ? 10 : 32, color: Colors.white, fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),)),
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        if (supabase.auth.currentUser == null) {
+                          Navigator.pushNamed(context, '/sign',
+                                  arguments: "profile")
+                              .then((value) => setState(() {}));
+                        } else {
+                          Navigator.pushNamed(context, '/profile')
+                              .then((value) => setState(() {}));
+                        }
+                      },
+                      icon: supabase.auth.currentSession == null
+                          ? CircleAvatar(
+                              child: Icon(Icons.person),
+                              backgroundColor: Colors.white,
+                              radius: 100,
+                            )
+                          : BoringAvatars(
+                              name: supabase.auth.currentUser!.email!)),
+                  IconButton(
+                      onPressed: () {
+                        launchUrl(Uri.parse(
+                            "https://github.com/forecaster-cyber/Meshik"));
+                      },
+                      icon: Icon(Icons.info)),
+                  
+                ],
+                bottom: const TabBar(
+                  tabs: [
+                    Tab(
+                      text: "שיחות פרטיות",
+                    ),
+                    Tab(
+                      text: "לוח שאלות",
+                    ),
+                  ],
                 ),
-                Tab(
-                  text: 'שיחות פרטיות',
-                ),
-              ],
-            ),
-          ),
-            ))
-        ),
+              ),
+            ))),
         body: const TabBarView(
           children: [
-            QuestionsBoard(),
             calls_screen(),
+            QuestionsBoard(),
           ],
         ),
       ),
